691849a38cb1ca13d9dd4886bf4443ac96f74b6d
Lucky <sahulucky3238@gmail.com>
2022-02-12 19:36:00 +0530

    adding message in diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+ int main()
+ {
+     int a=8,b=4;
+
+     int diff = a-b;
+     cout <<" the  difference is  : "<< diff << endl;
+
+     return 0;
+     
+ }
