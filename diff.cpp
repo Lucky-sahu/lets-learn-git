aa7e8fe029345af593e0919f551d24891737d216
Lucky <sahulucky3238@gmail.com>
2022-02-12 19:51:23 +0530

    adding missing thing

--- diff.cpp
+++ diff.cpp
@@ -6,7 +6,7 @@ using namespace std;
      int a=8,b=4;
 
      int diff = a-b;
-     cout <<" the  difference is  : "<< diff << endl;
+     cout <<" the  difference  of two no. is  : "<< diff << endl;
 
      return 0;
      
